61b223f03ce72bc99ca49555944d636bf5da2356
BonewheelMaster <bonewheelmaster@proton.me>
2026-09-17 17:31:54 -0400

    Add reload functionality upon changing type of NPC

--- src/settings-menu.ts
+++ src/settings-menu.ts
@@ -84,6 +84,7 @@ const menu = async () => {
 
     npcTypeDropDown.addEventListener("change", () => {
         npcOps.changeType(npcTypeDropDown.value as state.NPCAIType, selNPCs);
+        menu(); // To reload the type-specific settings
     });
 
     switch (NPCType) {
--- tscdump/settings-menu.js
+++ tscdump/settings-menu.js
@@ -77,6 +77,7 @@ const menu = async () => {
     }
     npcTypeDropDown.addEventListener("change", () => {
         npcOps.changeType(npcTypeDropDown.value, selNPCs);
+        menu(); // To reload the type-specific settings
     });
     switch (NPCType) {
         case "None": return;
